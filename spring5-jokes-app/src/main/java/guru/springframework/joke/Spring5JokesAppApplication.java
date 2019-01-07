60c78d80d9dcf8d8ee5c73482831b9cd6fb42d4b
Tomasz Drwięga <tomdrw88@o2.pl>
2019-01-07 22:25:44 +0100

    Spring5-jokes-app initial commit

---
+++ spring5-jokes-app/src/main/java/guru/springframework/joke/Spring5JokesAppApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+
+}
+
